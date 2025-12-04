4321a1daf3aca7c97fabac6c408a40f026a9a6de
bisht95 <bisht.gaurav95@gmail.com>
2025-12-04 15:59:07 +0530

    add alert msg for mode light and dark

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 //import logo from './logo.svg';
 import './App.css';
+
 //import About from './component/About';
 import Mainbanner from './component/mainbanner';
+import Alert from './component/Alert';
 // import MyUppercase from './component/myuppercase';
 import Navbar from './component/navbar';
 import TextForm from './component/textform';
@@ -13,7 +15,19 @@ function App() {
 
   const [mode, setMode] = useState('light');
   const [toggleText, setToggleText]= useState('Dark Mode Enable')
+  const [alert, setAlert] = useState(null)
 
+  const showAlert = (massage, type ) => {
+    setAlert({
+      msg:massage,
+      type:type
+    })
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500)
+
+  }
 
   const toggleMode = () => {
       if(mode === 'light')
@@ -21,22 +35,26 @@ function App() {
         setMode('dark')
         document.body.style.backgroundColor = '#042743';
         setToggleText('Light Mode Enable')
+        showAlert("Dark mode has been enabled", "success");
       }
 
       else{
         setMode('light');
         document.body.style.backgroundColor = 'white';
         setToggleText('Dark Mode Enable')
+        showAlert("Light mode has been enabled", "success");
       }
   }
 
-  
+
 
   return (
     <div>
-      <Navbar toggleMode={toggleMode} mode={mode} toggleText={toggleText}/>
+      <Navbar toggleMode={toggleMode} mode={mode} toggleText={toggleText} />
+      <Alert alert={alert}/>
       <Mainbanner name="Test" subheading="This is Subheading" mode={mode}/>
       <TextForm textarea_lable="This is Text Case Converter" mode={mode}/>
+
       {/* <MyUppercase uppercaseheading="This Uppercase com"/> */}
       {/* <About theme_title='Theme Mode'/> */}
     </div>
